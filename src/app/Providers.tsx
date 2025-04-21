f4576311b0f677d930749cac2fc34908ad4c7264
Blankaert <blankaertlouis@outlook.com>
2025-04-21 18:36:34 +0200

    first commit

---
+++ src/app/Providers.tsx
@@ -0,0 +1,22 @@
+// app/Providers.tsx
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from 'next-themes';
+import { PropsWithChildren } from 'react';  
+import { Toaster, toast } from 'sonner';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+
+export const Providers = ({ children }: PropsWithChildren) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Toaster />
+        <SessionProvider>{children}</SessionProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
